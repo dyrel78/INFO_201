2cc089f2eb5c49093fbaf8afa56349f512e241d6
lumdy690 <lumdy690@student.otago.ac.nz>
2023-05-08 15:41:52 +1200

    Started Milestone 3

---
+++ milestone3/app/src/main/java/milestone3/domain/Customer.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package milestone3.domain;
+
+/**
+ *
+ * @author dyrellumiwes
+ */
+
+public class Customer {
+    //Data fields
+    private Integer customerId;
+    private String username;
+    private String firstName;
+    private String surname;
+    private String password;
+    private String emailAddress;
+    private String shippingAddress;
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(String shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+}
---
+++ milestone3/app/src/main/java/milestone3/domain/Sale.java
@@ -0,0 +1,88 @@
+package milestone3.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dyrellumiwes
+ */
+
+
+
+
+public class Sale {
+ private Integer saleID;
+    private LocalDateTime date;
+    private String status
+    private Customer customer;
+    private ArrayList<SaleItem> items;  
+    
+    
+    public BigDecimal getTotal(){
+        BigDecimal total = new BigDecimal(0);
+        
+        for(int i = 0; i< items.size;i++){
+           BigDecimal itemTotal = items.get(i).getItemTotal();
+                total.add(itemTotal);
+
+        }
+        return total;
+    }
+    
+    public void addItem(SaleItem saleItem){
+        
+        items.add(saleItem);
+    }
+    
+    public Integer getSaleID() {
+        return saleID;
+    }
+
+    public void setSaleID(Integer saleID) {
+        this.saleID = saleID;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Collection<SaleItem> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<SaleItem> items) {
+        this.items = items;
+    }
+    
+     
+    
+}
+
